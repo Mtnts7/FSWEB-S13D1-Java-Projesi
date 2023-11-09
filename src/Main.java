b27d5cdfda84603fdfd3ac5175aff1036144c098
Mehmet Ali Metintas <mehmet.ali.metintas@gmail.com>
2023-11-09 15:01:54 +0300

    odev tamam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(shouldWakeUp (true, 1));
+        System.out.println(shouldWakeUp (false, 2));
+        System.out.println(shouldWakeUp (true, 8));
+        System.out.println(shouldWakeUp (true, -1));
+
     }
+
+    public static boolean shouldWakeUp(boolean havliyorMu, int saat) {
+        if(saat<0 || saat>23){
+            return false;
+        }
+        if(havliyorMu){
+            if(saat>=8 && saat<=20){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
